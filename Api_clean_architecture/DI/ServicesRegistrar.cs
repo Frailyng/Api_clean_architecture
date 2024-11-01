323f455766b77a86ce40d016fc9da848fee9d390
frailyng <frailindes@gmail.com>
2024-11-01 16:44:13 -0400

    ServiceRegistrar Creado

--- Api_clean_architecture/DI/ServicesRegistrar.cs
+++ Api_clean_architecture/DI/ServicesRegistrar.cs
@@ -1,8 +1,13 @@
-﻿namespace Api_clean_architecture.DI;
+﻿using Api_clean_architecture.DIS;
 
-public class ServicesRegistrar
+namespace Api_clean_architecture.DI;
+
+public static class ServicesRegistrar
 {
     public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        services.RegisterDbContextFactory();
+        services.AddScoped<IClientesServices, ClientesService>();
+        return services;
     }
 }
