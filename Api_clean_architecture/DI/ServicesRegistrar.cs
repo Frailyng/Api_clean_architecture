66e954ec26e07ad196b994f63f1b10734a22788b
frailyng <frailindes@gmail.com>
2024-11-01 16:42:09 -0400

    DbContextRegistrarCreado para} usar DbContextFactory

--- Api_clean_architecture/DI/ServicesRegistrar.cs
+++ Api_clean_architecture/DI/ServicesRegistrar.cs
@@ -1,6 +1,8 @@
-﻿namespace Api_clean_architecture.DI
+﻿namespace Api_clean_architecture.DI;
+
+public class ServicesRegistrar
 {
-    public class ServicesRegistrar
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
     }
 }
---
+++ Api_clean_architecture/DIS/DbContextRegistrar.cs
@@ -0,0 +1,13 @@
+﻿using Api_clean_architecture.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_clean_architecture.DIS;
+
+public static class DbContextRegistrar
+{
+    public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+    {
+        services.AddDbContextFactory<TecnicosContext>(o => o.UseSqlServer("Name=SqlConStr"));
+        return services;
+    }
+}
