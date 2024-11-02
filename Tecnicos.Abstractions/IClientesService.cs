3941c596018862ab1a19d319bb954638602e0913
frailyng <frailindes@gmail.com>
2024-11-02 15:28:25 -0400

    Iclientes Service Creado

--- Tecnicos.Abstractions/IClientesService.cs
+++ Tecnicos.Abstractions/IClientesService.cs
@@ -1,7 +1,14 @@
-﻿namespace Tecnicos.Abstractions
-{
-    public class IClientesService
-    {
+﻿using System.Linq.Expressions;
+using Tecnicos.Domain.DTOS;
 
-    }
+namespace Tecnicos.Abstractions;
+
+public interface IClientesService
+{
+    Task<bool> Guardar(ClientesDto cliente);
+    Task<bool> Eliminar(int clienteId);
+    Task<ClientesDto> Buscar(int id);
+    Task<List<ClientesDto>> Listar(Expression<Func<ClientesDto, bool>> criterio);
+    Task<bool> ExisteCliente(int id, string nombres, string whatsapp);
+
 }
